b5a11245e89091f9a7dd18bd9be8fd562c30aef3
Victor Tran <victor-likes-coding@tempestas.io>
2022-04-21 07:26:41 -0400

    write test for fn that returns arr length

--- app.test.js
+++ app.test.js
@@ -11,3 +11,15 @@ describe("fn to return first input if first input is falsy else second input", (
         expect(filterOutFalsy(true, "Dog")).toBe("Dog");
     });
 });
+
+describe("fn that returns length of array (stupid)", () => {
+    test("in: [1,2,3] out: 3", () => {
+        expet(arrLength([1, 2, 3])).toBe(3);
+    });
+    test("in: [5,0,-4,1] out: 4", () => {
+        expet(arrLength([5, 0, -4, 1])).toBe(4);
+    });
+    test("in: [] out: 0", () => {
+        expet(arrLength([])).toBe(0);
+    });
+});
