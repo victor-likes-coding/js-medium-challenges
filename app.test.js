7e2bfabb6a2358dbf8b49e6974a511683dbb6ed1
Victor Tran <victor-likes-coding@tempestas.io>
2022-04-21 07:00:46 -0400

    add test file and write first test for falsy fn

---
+++ app.test.js
@@ -0,0 +1,11 @@
+describe("fn to return first input if first input is falsy else second input", () => {
+    test("in: 0, 500 out: 0", () => {
+        expect(filterOutFalsy(0, 500)).toBe(0);
+    });
+    test("in: false, 100 out: 0", () => {
+        expect(filterOutFalsy(false, 100)).toBe(false);
+    });
+    test("in: true, 'Dog' out: 0", () => {
+        expect(filterOutFalsy(true, "Dog")).toBe("Dog");
+    });
+});
