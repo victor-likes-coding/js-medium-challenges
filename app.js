ce79c1eb9f47725eda5f13d28644db9854bdebe7
Victor Tran <victor-likes-coding@tempestas.io>
2022-04-28 07:04:58 -0400

    write fn to return arr length

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const filterOutFalsy = (in1, in2) => (in1 ? in2 : in1);
 
+const arrLength = (arr = []) => {
+    return arr.length;
+};
+
 module.exports = {
     filterOutFalsy,
+    arrLength,
 };
