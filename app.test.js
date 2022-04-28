de8494c2acb5c068cbfe4a1292d6ac707ce90d31
Victor Tran <victor-likes-coding@tempestas.io>
2022-04-28 07:05:06 -0400

    import arrLength fn to test, passed

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const { filterOutFalsy } = require("./app.js");
+const { filterOutFalsy, arrLength } = require("./app.js");
 
 describe("fn to return first input if first input is falsy else second input", () => {
     test("in: 0, 500 out: 0", () => {
@@ -14,12 +14,12 @@ describe("fn to return first input if first input is falsy else second input", (
 
 describe("fn that returns length of array (stupid)", () => {
     test("in: [1,2,3] out: 3", () => {
-        expet(arrLength([1, 2, 3])).toBe(3);
+        expect(arrLength([1, 2, 3])).toBe(3);
     });
     test("in: [5,0,-4,1] out: 4", () => {
-        expet(arrLength([5, 0, -4, 1])).toBe(4);
+        expect(arrLength([5, 0, -4, 1])).toBe(4);
     });
     test("in: [] out: 0", () => {
-        expet(arrLength([])).toBe(0);
+        expect(arrLength([])).toBe(0);
     });
 });
